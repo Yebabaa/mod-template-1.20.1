41059d026fd96c73108865c63327802c38df1e51
Yebaba <mario.richti@gmail.com>
2025-04-29 16:41:56 +0200

    Initial commit

---
+++ src/main/java/com/mario/mod/ModClient.java
@@ -0,0 +1,11 @@
+package com.mario.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
