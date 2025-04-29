8df25461b55e08b6fac81c8e8c91167ec02097b0
Yebaba <mario.richti@gmail.com>
2025-04-29 18:44:14 +0200

    added Ruby and stuff idk

--- src/main/java/com/mario/mod/item/ModItems.java
+++ src/main/java/com/mario/mod/item/ModItems.java
@@ -2,10 +2,7 @@ package com.mario.mod.item;
 
 import com.mario.mod.Mod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
